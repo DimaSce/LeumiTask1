e4c204a8fe9634676104140f9bab16c10e912144
DimaSce <58255278+DimaSce@users.noreply.github.com>
2023-03-03 22:06:24 +0200

    Update app.py

--- project/app.py
+++ project/app.py
@@ -9,7 +9,7 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 api = Api(
     app,
     version="1.2",
-    title="Calculator APIAPI",
+    title="Calculator API2.0",
     description="A simple calculator API that performs basic mathematical operations",
 )
 
